17505d121b0e2fa042ba6d18533461f110d6baac
Luka-Penida <lpenida1@mail.austral.edu.ar>
2025-09-08 11:07:41 -0300

    Add files via upload

--- maximums.py
+++ maximums.py
@@ -2,9 +2,11 @@
 
 def max_of_two(x, y):
     """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
+    return max(x,y) # Remove this line and implement
+print(max_of_two(-1,3))
 
 
 def max_of_three(x, y, z):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    return max(x,y,z) # Remove this line and implement
+print(max_of_three(-1,-2,5))
